4f12bc173db00b68d018dc50ffb2e3223e78f877
Derek Knechtel <derek.knechtel@gmail.com>
2017-04-13 11:24:30 -0500

    Changed database creation to work

--- models/database.js
+++ models/database.js
@@ -12,21 +12,12 @@
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/beers';
 var client = new pg.Client(connectionString);
-
-var execQuery = function(query) {
-    client.query(query, function(err, result) {
-        console.log(query);
-        if (err) throw err;
-        console.log(result);
-    });
-};
-
+client.on("drain", client.end.bind(client));
 client.connect();
 console.log("Starting creating tables...");
-execQuery("CREATE TABLE beer(id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, type VARCHAR(30))");
-execQuery("CREATE TABLE brewery(id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, established INTERVAL YEAR, location VARCHAR(30))");
-execQuery("CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, created TIMESTAMP)");
-execQuery("CREATE TABLE brewed(beer_id INTEGER REFERENCES beer (id), brewery_id INTEGER REFERENCES brewery (id), first_brewed DATE, PRIMARY KEY (beer_id, brewery_id))");
-execQuery("CREATE TABLE post(id SERIAL PRIMARY KEY, description TEXT NOT NULL, rating INTEGER, user_id INTEGER REFERENCES users (id), beer_id INTEGER REFERENCES beer (id))");
+client.query("CREATE TABLE beer(id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, type VARCHAR(30))");
+client.query("CREATE TABLE brewery(id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, established INTERVAL YEAR, location VARCHAR(30))");
+client.query("CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(30) NOT NULL, created TIMESTAMP)");
+client.query("CREATE TABLE brewed(beer_id INTEGER REFERENCES beer (id), brewery_id INTEGER REFERENCES brewery (id), first_brewed DATE, PRIMARY KEY (beer_id, brewery_id))");
+client.query("CREATE TABLE post(id SERIAL PRIMARY KEY, description TEXT NOT NULL, rating INTEGER, user_id INTEGER REFERENCES users (id), beer_id INTEGER REFERENCES beer (id))");
 console.log("Finished creating tables");
-client.end();
