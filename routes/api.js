aef0b841b5cb5c631fed95add8646c58d02a0f71
Derek Knechtel <derek.s.knechtel@intel.com>
2017-04-17 23:07:07 -0500

    Added routes to brewery. Some minor refactoring.

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,17 @@ const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/beers';
 var client = new pg.Client(connectionString);
 
+
+var queryMap = {};
+queryMap["beer"] = {
+    "GET": "SELECT * FROM beer",
+    "POST": "INSERT INTO beer (name, type) VALUES ($1, $2)"
+};
+queryMap["brewery"] = {
+    "GET": "SELECT * FROM brewery",
+    "POST": "INSERT INTO brewery (name, established) VALUES ($1, $2)"
+};
+
 function getResult(query, res) {
     client.connect(function(err) {
         if (err) throw err;
@@ -14,18 +25,35 @@ function getResult(query, res) {
     })
 };
 
-/* GET home page. */
+function postResult(query, arr, res) {
+    client.connect(function (err, client, done) {
+        if (err) throw err;
+        client.query(query, arr);
+        res.send("POST Okay");
+    });
+}
+
+router.get('/', function(req, res, next) {
+    res.send("Default entry to API");
+});
+
+
 router.get('/beers', function(req, res, next) {
-    getResult("SELECT * FROM beer", res);
+    getResult(queryMap["beer"]["GET"], res);
 });
 
 router.post('/beers', function(req, res, next) {
     const data = {name: req.body.name, type: req.body.type};
-    client.connect(function (err, client, done) {
-        if (err) throw err;
-        client.query("INSERT INTO beer (name, type) VALUES ($1, $2)", [data.name, data.type]);
-        res.send("POST Okay");
-    });
+    postResult(queryMap["beer"]["POST"], [data.name, data.type]);
+});
+
+router.get('/brewery', function(req, res, next) {
+    getResult(queryMap["brewery"]["GET"], res);
+});
+
+router.post('/brewery', function(req, res, next) {
+    const data = {name: req.body.name, established: req.body.established};
+    postResult(queryMap["brewery"]["POST"], [data.name, data.established]);
 });
 
 module.exports = router;
