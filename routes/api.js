a843b14451e3c4d5909468738fe549f5caa8f257
Derek Knechtel <derek.knechtel@gmail.com>
2017-04-19 22:28:38 -0500

    Changed client to pool for api routing. Minor refactoring. Changed GET requests to specify IDs

--- routes/api.js
+++ routes/api.js
@@ -2,24 +2,32 @@ var express = require('express');
 var router = express.Router();
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/beers';
-var client = new pg.Client(connectionString);
+var config = {
+  user: 'Doko', //env var: PGUSER 
+  database: 'beers', //env var: PGDATABASE 
+  host: 'localhost', // Server hosting the postgres database 
+  port: 5432, //env var: PGPORT 
+  max: 10, // max number of clients in the pool 
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+};
+var pool = new pg.Pool(config);
 
 
 var queryMap = {};
 queryMap["beer"] = {
-    "GET": "SELECT * FROM beer",
-    "POST": "INSERT INTO beer (name, type) VALUES ($1, $2)"
+    "GET": "SELECT * FROM beer where id=$1",
+    "POST": "INSERT INTO beer (name, type) VALUES ($1, $2) returning id"
 };
 queryMap["brewery"] = {
-    "GET": "SELECT * FROM brewery",
-    "POST": "INSERT INTO brewery (name, established) VALUES ($1, $2)"
+    "GET": "SELECT * FROM brewery where id=$1",
+    "POST": "INSERT INTO brewery (name, established) VALUES ($1, $2) returning id"
 };
 
-function getResult(query, res) {
-    client.connect(function(err) {
+function getResult(query, res, id) {
+    pool.connect(function(err, client, done) {
         if (err) throw err;
-        client.query(query, function(err, result) {
-            if (err) throw err;
+        client.query(query, [id], function(error, result) {
+            if (error) throw error;
             res.send(result["rows"]);
         });
     })
@@ -28,7 +36,10 @@ function getResult(query, res) {
 function postResult(query, arr, res) {
     client.connect(function (err, client, done) {
         if (err) throw err;
-        client.query(query, arr);
+        client.query(query, arr, function(error, result) {
+            if (error) throw error;
+            console.log(result);
+        });
         res.send("POST Okay");
     });
 }
@@ -39,7 +50,7 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/beers', function(req, res, next) {
-    getResult(queryMap["beer"]["GET"], res);
+    res.send("Get entry to Beers");
 });
 
 router.post('/beers', function(req, res, next) {
@@ -47,8 +58,12 @@ router.post('/beers', function(req, res, next) {
     postResult(queryMap["beer"]["POST"], [data.name, data.type], res);
 });
 
+router.get("/beers/:id", function(req, res) {
+    getResult(queryMap["beer"]["GET"], res, req.params.id);
+});
+
 router.get('/brewery', function(req, res, next) {
-    getResult(queryMap["brewery"]["GET"], res);
+    getResult(queryMap["brewery"]["GET"], res, req.params.id);
 });
 
 router.post('/brewery', function(req, res, next) {
